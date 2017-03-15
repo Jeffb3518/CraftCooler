556f5e2d5bf5df40907d021bfb246880fa803e12
Jeffrey Bauer <jeffrey.bauer05@gmail.com>
2017-03-15 10:52:07 -0400

    frontEnd API js

--- public/js/frontEndData.js
+++ public/js/frontEndData.js
@@ -10,7 +10,19 @@ $('#runSearch').on('click', function(){
 $.post("/beerData", userData, function(data) {
 
    for(var i = 0; i < data.length; i++) {
-       $("div").append(data[i].brewery.name)
+       var beerDiv = $("<div class=\"beer-item\">");
+
+       var name = data[i].brewery.name
+       var location = data[i].locality
+       var state = data[i].region
+       var description = data[i].brewery.description
+       var beerName = data[i].name
+
+       var nameResult = $("<p>").text("Brewery Name: " + brewery.name);
+       var locationResult = $("<p>").text("City: " + locality);
+       var stateResult = $("<p>").text("State: " + region);
+       var descriptionResult = $("<p>").text("Description: " + brewery.description);
+       var beerNameResult = $("<p>").text("Name: " + name);
    }
 
 console.log(data);
