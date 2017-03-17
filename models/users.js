390118fd12579ebe3061adade54427fa88d4cc9c
Jeffrey Bauer <jeffrey.bauer05@gmail.com>
2017-03-17 14:35:34 -0400

    major authentication work

--- models/users.js
+++ models/users.js
@@ -1,26 +1,57 @@
-module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
+// module.exports = function(sequelize, DataTypes) {
+//     var User = sequelize.define("User", {
        
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            isAlpha: true
+//         first_name: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             isAlpha: true
+//         },
+//         last_name: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             isAlpha: true
+//         },
+//         email: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             isEmail: true
+//         },
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             len: [6, 15]
+//         }
+//     });
+//     return User;
+// };
+
+// Requiring bcrypt for password hashing. Using the bcrypt version as the regular bcrypt module
+var bcrypt = require("bcrypt-nodejs");
+// Creating our User model
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    first: {
+        type: DataTypes.STRING,
+        allowNull: false
         },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            isAlpha: true
+    last: {
+        type: DataTypes.STRING,
+        allowNull: false
         },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            isEmail: true
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            len: [6, 15]
-        }
-    });
-    return User;
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return User
 };
